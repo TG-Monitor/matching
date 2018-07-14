3a0bb81a1d65879edd8ae64afb054c0b873021ab
Daniel Weibel <danielmweibel@gmail.com>
2018-07-14 18:08:28 +0800

    Adapt to new service locator architecture

--- src/main/java/ai/quantumsense/tgmonitor/matching/PatternMatcherImpl.java
+++ src/main/java/ai/quantumsense/tgmonitor/matching/PatternMatcherImpl.java
@@ -1,39 +1,33 @@
 package ai.quantumsense.tgmonitor.matching;
 
-import ai.quantumsense.tgmonitor.backend.InteractorFactory;
+import ai.quantumsense.tgmonitor.backend.Interactor;
 import ai.quantumsense.tgmonitor.backend.PatternMatcher;
 import ai.quantumsense.tgmonitor.backend.pojo.PatternMatch;
 import ai.quantumsense.tgmonitor.backend.pojo.TelegramMessage;
-import ai.quantumsense.tgmonitor.monitor.data.MonitorDataFactory;
+import ai.quantumsense.tgmonitor.entities.Patterns;
+import ai.quantumsense.tgmonitor.servicelocator.ServiceLocator;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class PatternMatcherImpl implements PatternMatcher {
 
-    private InteractorFactory interactorFactory;
-    private MonitorDataFactory monitorDataFactory;
+    private ServiceLocator<Interactor> interactorLocator;
+    private ServiceLocator<Patterns> patternsLocator;
 
-    public PatternMatcherImpl(InteractorFactory interactorFactory, MonitorDataFactory monitorDataFactory) {
-        this.monitorDataFactory = monitorDataFactory;
-        this.interactorFactory = interactorFactory;
+    public PatternMatcherImpl(ServiceLocator<Interactor> interactorLocator, ServiceLocator<Patterns> patternsLocator) {
+        this.interactorLocator = interactorLocator;
+        this.patternsLocator = patternsLocator;
     }
 
     @Override
     public void newMessage(TelegramMessage msg) {
-        String text = msg.getText();
         Set<String> matches = new HashSet<>();
-
-        for (String p : patterns()) {
-            if (text.contains(p)) matches.add(p);
+        for (String p : patternsLocator.getService().getPatterns()) {
+            if (msg.getText().contains(p)) matches.add(p);
         }
-
         if (matches.size() > 0) {
-            interactorFactory.getInteractor().matchFound(new PatternMatch(msg, matches));
+            interactorLocator.getService().matchFound(new PatternMatch(msg, matches));
         }
-    }
-
-    private Set<String> patterns() {
-        return monitorDataFactory.getMonitorData().getPatterns();
     }
 }
